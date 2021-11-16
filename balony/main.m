c9f4b7051952a9ac75af046bf4a2e4085a6f4f5e
bostrow99 <ostrowski.bartosz.99@gmail.com>
2021-11-17 00:50:35 +0100

    coś

--- balony/main.m
+++ balony/main.m
@@ -3,19 +3,22 @@ clear;
 clc;
 
 fs = 44100;
-
+Guitar = Response("guitar.wav","f",fs);
 piezo = Response("piezo.wav","f",fs);
+GuitarPiezo = Guitar.filter(piezo.h,"t");
 
-DelayTemp = zeros(1,50000);
+DelayTemp = zeros(0.5e5,1);
 DelayTemp(1,1) = 1;
-DelayTemp(1,2e4) = 1;
+DelayTemp(end-1,1) = 0.5;
 
 Delay = Response(DelayTemp,"t",fs);
 Delay.time("t");
 
 Reverb1 = Response("gg aula/test5.wav","b",fs);
 
-Guitar = Response("guitar.wav","f",fs);
 GuitarReverb = Guitar.filter(Reverb1.H,"f");
 
-GuitarReverb.play;
+GuitarDelay = Guitar.filter(Delay.H,"f");
+
+
+%GuitarReverb.play;
